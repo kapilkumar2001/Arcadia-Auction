c931340089cebc067113d19b6b1a351510601d6e
nishant-madara <andoriyanishant@gmail.com>
2021-08-24 12:31:41 +0530

    announcement UI done

--- lib/screens/Player/PlayerScreens/update_screen.dart
+++ lib/screens/Player/PlayerScreens/update_screen.dart
@@ -5,6 +5,7 @@ import 'package:arcadia/provider/announcements.dart';
 import 'package:arcadia/provider/player.dart';
 import 'package:arcadia/widgets/announcement_card.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
@@ -37,62 +38,70 @@ class _UpdateScreenState extends State<UpdateScreen> {
   Widget build(BuildContext context) {
     announcementList =
         Provider.of<Announcements>(context, listen: false).announcement;
-    print('announcements :   $announcementList');
+    // print('announcements :   $announcementList');
     return _isLoading
         ? Center(
             child: CircularProgressIndicator(),
           )
-        : SingleChildScrollView(
-            child: SafeArea(
-              child: Column(
-                children: [
-                  Container(
-                    height: 100,
-                    padding: EdgeInsets.all(30),
-                    child: Text(
-                      'Arcadia CSGO League',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 30,
+        : SafeArea(
+            child: SingleChildScrollView(
+              child: Container(
+                color: CustomColors.firebaseNavy,
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(),
+                      width: MediaQuery.of(context).size.width,
+                      // height: 100,
+                      padding: EdgeInsets.all(30),
+                      child: Text(
+                        'Arcadia CSGO League',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.lato(
+                          // textStyle: Theme.of(context).textTheme.headline4,
+                          fontSize: 32,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          // fontStyle: FontStyle.italic,
+                        ),
                       ),
                     ),
-                  ),
-                  Container(
-                    height: MediaQuery.of(context).size.height-200,
-                    child: ListView(
-                    
-                      children:[ 
-                        ...announcementList.map((e) => AnnouncementCard(announcement:e )).toList(),
+                    // SizedBox(
+                    //   height: 20,
+                    // ),
+                     Container(
+                      decoration: BoxDecoration(
                         
-                      // ExpansionTile(
-                      //   title: Text(
-                      //     announcementList[0].title.toString(),
-                      //     style: TextStyle(
-                      //         color: Colors.black,
-                      //         fontWeight: FontWeight.bold,
-                      //         fontSize: 22),
-                      //   ),
-                      //   subtitle: Text(
-                      //     announcementList[0].subtitle.toString(),
-                      //     style: TextStyle(
-                      //         color: Colors.black,
-                      //         fontWeight: FontWeight.bold,
-                      //         fontSize: 15),
-                      //   ),
-                      //   children: [
-                      //     Text(
-                      //       announcementList[0].desc.toString(),
-                      //       style: TextStyle(
-                      //           color: Colors.black,
-                      //           fontWeight: FontWeight.bold,
-                      //           fontSize: 15),
-                      //     ),
-                      //   ],
-                      // ),
-                      ]
+                      ),
+                      width: MediaQuery.of(context).size.width,
+                      // padding: EdgeInsets.all(30),
+                      child: Text(
+                        'Annoucements',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.lato(
+                          // textStyle: Theme.of(context).textTheme.headline4,
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                          // fontStyle: FontStyle.italic,
+                        ),
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      color: CustomColors.firebaseNavy,
+                      height: MediaQuery.of(context).size.height - 200,
+                      child: ListView(children: [
+                        ...announcementList
+                            .map((e) => AnnouncementCard(announcement: e))
+                            .toList(),
+                      ]),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
